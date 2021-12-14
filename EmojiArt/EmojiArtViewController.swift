ed5bdf1b80817794927fd67637a3de2e290e14fb
Gabriel <gabrieldenonifake@gmail.com>
2021-12-14 13:45:38 -0300

    Create the project.

---
+++ EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by Gabriel on 12/14/21.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
