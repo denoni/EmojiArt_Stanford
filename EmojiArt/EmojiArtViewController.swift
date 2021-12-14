6e07c7cf0a0aba159a51f391a59624b7cde9f96a
Gabriel <gabrieldenonifake@gmail.com>
2021-12-14 17:42:46 -0300

    Make the fetched image be scrollable

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
+
+  var emojiArtView = EmojiArtView()
 
   @IBOutlet weak var dropZone: UIView! {
     didSet {
       dropZone.addInteraction(UIDropInteraction(delegate: self))
     }
   }
+
+  @IBOutlet weak var scrollView: UIScrollView! {
+    didSet {
+      scrollView.minimumZoomScale = 0.1
+      scrollView.maximumZoomScale = 5.0
+      scrollView.delegate = self
+      scrollView.addSubview(emojiArtView)
+    }
+  }
+
+  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+    return emojiArtView
+  }
+
+  var imageFetcher: ImageFetcher!
 
   // Makes sure the dropped item is an NSURL and UIImage
   func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
@@ -25,14 +42,12 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
     return UIDropProposal(operation: .copy)
   }
 
-  var imageFetcher: ImageFetcher!
-
   // Drop and fetch the image
   func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
 
     imageFetcher = ImageFetcher() { (url, image) in
       DispatchQueue.main.async {
-        self.emojiArtView.backgroundImage = image
+        self.emojiArtBackgroundImage = image
       }
     }
 
@@ -49,7 +64,23 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
     }
   }
 
-  @IBOutlet weak var emojiArtView: EmojiArtView!
+
+  var emojiArtBackgroundImage: UIImage? {
+    get {
+      return emojiArtView.backgroundImage
+    }
+    set {
+      scrollView?.zoomScale = 1
+      emojiArtView.backgroundImage = newValue
+      let size = newValue?.size ?? CGSize.zero
+      emojiArtView.frame = CGRect(origin: CGPoint.zero, size: size)
+      scrollView?.contentSize = size
+      if let dropZone = self.dropZone, size.width > 0, size.height > 0 {
+        scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width,
+                                    dropZone.bounds.size.height / size.height)
+      }
+    }
+  }
 
 }
 
