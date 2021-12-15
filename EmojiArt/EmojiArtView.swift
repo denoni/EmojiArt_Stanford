a71958e9aea979719e1977a3ea7b18b109d2cd53
Gabriel <gabrieldenonifake@gmail.com>
2021-12-15 00:20:31 -0300

    Now the image can support emojis being dropped in it

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
-class EmojiArtView: UIView {
+class EmojiArtView: UIView, UIDropInteractionDelegate {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setup()
+  }
+
+  private func setup() {
+    addInteraction(UIDropInteraction(delegate: self))
+  }
 
   var backgroundImage: UIImage? { didSet { setNeedsDisplay() } }
   
@@ -15,5 +29,30 @@ class EmojiArtView: UIView {
     backgroundImage?.draw(in: bounds)
   }
 
+  func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+    return session.canLoadObjects(ofClass: NSAttributedString.self)
+  }
 
+  func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+    return UIDropProposal(operation: .copy)
+  }
+
+  func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+    session.loadObjects(ofClass: NSAttributedString.self) { providers in
+      let dropPoint = session.location(in: self)
+      for attributedString in providers as? [NSAttributedString] ?? [] {
+        self.addLabel(with: attributedString, centeredAt: dropPoint)
+      }
+    }
+  }
+
+  private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
+    let label = UILabel()
+    label.backgroundColor = .clear
+    label.attributedText = attributedString
+    label.sizeToFit()
+    label.center = point
+    addEmojiArtGestureRecognizers(to: label)
+    addSubview(label)
+  }
 }
